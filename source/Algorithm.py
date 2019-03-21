edd7d7fc210c47730a6f61b60bb5401a19423ef1
Milkweedz <Milkweedz@hotmail.com>
2019-03-21 15:19:15 +1100

    Modify Algorithm
    Functions added:
    main()
    #used for testing functions in Algorithm
    
    Functions changed:
    heuristic(coords, goal)
    #modified modifier on heuristic value h(n) to be closer to h*(n)
    
    Functions removed:

--- source/Algorithm.py
+++ source/Algorithm.py
@@ -2,6 +2,12 @@
 #specify axes names of the three values in coordinates tuple passed to heuristic()
 q,r,s = 0,1,2
 
+def main():
+    global q, r, s
+    h = heuristic(((0,0,0),), (q,-3))
+    print(h, "DEBUG")
+
+    return None
 
 
 
@@ -14,13 +20,23 @@ def heuristic(coords, goal):
     goal_value = goal[1]    #either -n or n, where n is the radius of the hexagonal board
                             #   distinguishes between the two sides perpendicular to the goal_axis
 
+
     for coord in coords:
+        print(coords, "DEBUG")
         piece_distance = abs(coord[goal_axis] - goal_value)   #position of piece of goal_axis
         total_distance += piece_distance
 
 
     #modifier on heuristic value "h(n)"
-    h = total_distance/2    #set to 1/2 to account for pieces moving 2 squares by jumping
+    h = int(total_distance/2)+1     #set to 1/2 to account for pieces moving 2 squares by jumping
+                                    #int and +1 acts as ceiling function - since you cannot jump off the board
 
 
     return h
+
+
+
+
+
+if __name__ == "__main__":
+    main()
