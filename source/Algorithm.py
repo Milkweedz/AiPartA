04b0efa56933ddf118691967114e0d5d4f9a4d70
Milkweedz <Milkweedz@hotmail.com>
2019-03-23 00:50:30 +1100

    Modify Algorithm
    Functions added:
    path_finder(goal)
    # dummy function at the moment, future implementation to generate graph of piece states
    
    Functions changed:
    main()
    # renamed explored_nodes to explored_states
    # made board, explored_states, fringe_nodes global variables
    
    node_expander(goal)
    # renamed node to state
    # implemented tracking for g(n) - path cost to reach node's state
    # added f(n) = g(n) + h(n)
    
    Functions removed:

--- source/Algorithm.py
+++ source/Algorithm.py
@@ -1,33 +1,36 @@
 
-#specify axes names of the three values in coordinates tuple passed to heuristic()
-import asyncio
 import queue
 
 import operator
 
+# specify axes names of the three values in coordinates tuple passed to heuristic()
 q,r,s = 0,1,2
 board_radius = 3
 
 
-
+board = {}
+explored_states = {}                         # key: coordinate tuple
+fringe_nodes = queue.PriorityQueue()        # nodes that are adjacent to explored nodes, ordered by f(n)
 
 
 def main():
-    board = {(-3, 0, 3):"r", (-3, 1, 2):"r", (-3, 2, 1):"r", (-3, 3, 0):"r"}                                  #board key: coordinate tuple, data: piece color
-    explored_nodes = {}                         #key: coordinate tuple
-    fringe_nodes = queue.PriorityQueue()        #nodes that are adjacent to explored nodes, ordered by f(n)
-
+    global board, explored_states, fringe_nodes
+    # board key: coordinate tuple, data: piece color
+    # board = {(-3, 0, 3):"r", (-3, 1, 2):"r", (-3, 2, 1):"r", (-3, 3, 0):"r"}
+    board_input = {(-3, 0, 3): "r", (-3, 1, 2): "r", (-3, 2, 1): "r", (-3, 3, 0): "r", (-2, -1, 3): "r", (-2, 0, 2): "r", (-2, 1, 1): "r"}
+    board.update(board_input)
 
     global q, r, s
     h = heuristic(((0,0,0),(-1,0,3)), (q,-3))
     print("h(n) = ", h)
 
     goal = (q,3)
-    fringe_nodes.put((24, ((-3, 0, 3), (-3, 1, 2), (-3, 2, 1), (-3, 3, 0))))
-    node_expander(board, goal, explored_nodes, fringe_nodes)
+
+    fringe_nodes.put((24, 0, ((-3, 0, 3), (-3, 1, 2), (-3, 2, 1), (-3, 3, 0))))
+    node_expander(goal)
 
     while not fringe_nodes.empty():
-        print(fringe_nodes.get()[1])
+        print(fringe_nodes.get())
 
     return None
 
@@ -51,16 +54,20 @@ def heuristic(coords, goal):
     return h
 
 
-def node_expander(board, goal, explored_nodes, fringe_nodes):
+def node_expander(goal):
     global board_radius
+    global board, explored_states, fringe_nodes
+
     # node is the current state of your pieces on the board, index 1 removes priority value
-    node = fringe_nodes.get()[1]
-    explored_nodes[node] = ""
+    node = fringe_nodes.get()                                           # node contains f, g, and state
+    g = node[1]                                                         # g = cost of reaching state
+    state = node[2]                                                     # state = tuple of piece coordinates
+    explored_states[state] = ""
     moves = [(1,-1,0),(1,0,-1),(0,1,-1),(-1,1,0),(-1,0,1),(0,-1,1)]     # unit vectors of a piece's possible moves
     possible_moves = []                                                 # list of a piece's possible moves
     valid_moves = []                        # list of all possible moves for this turn, minus ones that are not legal
 
-    for piece in node:
+    for piece in state:
         possible_moves.clear()
 
         for move in moves:
@@ -76,15 +83,22 @@ def node_expander(board, goal, explored_nodes, fringe_nodes):
     # explore successors of current node, iterate through valid moves
     for choice in valid_moves:
         # choice[0] is original position of piece, index is index of piece in node tuple
-        index = node.index(choice[0])
+        index = state.index(choice[0])
         # choice[1] is new position of the piece, next_node is the new state
-        next_node = node[:index] + (choice[1],) + node[index+1:]
+        next_state = state[:index] + (choice[1],) + state[index+1:]
 
-        if next_node not in explored_nodes:
-            h = heuristic(coords=next_node, goal=goal)
-            fringe_nodes.put((h, next_node))
+        if next_state not in explored_states:
+            h = heuristic(coords=next_state, goal=goal)
+            # f is estimated total path cost, h is predicted cost of remaining path, g is current path cost
+            f = h + g
+            fringe_nodes.put((f, g + 1, next_state))
 
     return None
+
+
+def path_finder(goal):
+
+    pass
 
 
 if __name__ == "__main__":
