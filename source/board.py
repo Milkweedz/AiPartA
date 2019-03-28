50af746b5c7a18ced65ccace6dc0f00363afb5cb
Dandy <gellienathan@hotmail.com.au>
2019-03-28 14:34:22 +1100

    Added Board.py, with search.py and example methods, currently correctly sets up board and displays via print_board

---
+++ source/board.py
@@ -0,0 +1,44 @@
+
+BASE_SIZE = 3
+BLANK_SPACE = ""
+COLOUR_KEY = "colour"
+PIECES_KEY = "pieces"
+BLOCKS_KEY = "blocks"
+BLOCKS_STR = "[=]"
+
+
+board_dict = {}
+
+#give board own dictionary to store board state. set empty board
+def init():
+    # the dictionary to store these in
+    coord_range = range(-BASE_SIZE, BASE_SIZE)
+    # create board like in search.py
+    for coord in [(q,r) for q in coord_range for r in coord_range if -q-r in coord_range]:
+        board_dict[coord] = BLANK_SPACE
+
+# initialises board based on json data
+def setup(jsondata):
+    init()
+    print(jsondata.keys())
+    colour = jsondata.pop(COLOUR_KEY)
+    # read through file, add corresponding values to dictionary
+    for key in jsondata.keys():
+        for i in jsondata[key]:
+            assign_to_board(key, tuple(i), colour)
+
+# takes a key-value pair from json and adds it to board dictionary    
+def assign_to_board(key, value, colour):
+    
+    # sets each coordinate value to current colour
+    if key==PIECES_KEY:
+        board_dict[value]=colour
+    # sets each coord value to block
+    elif key==BLOCKS_KEY:
+        board_dict[value]=BLOCKS_STR
+
+
+    
+
+def get_board():
+    return board_dict
