966afaf2d8da8771d79d8e69e4b30d28d51a2fac
Milkweedz <Milkweedz@hotmail.com>
2019-03-30 16:52:34 +1100

    Modified Algorithm.py
    Functions added:
    dict_to_matrix(board)
    # converts board from dictionary to matrix to facilitate more efficient manipulation in heuristic()
    
    node_limit(current, limit)
    # returns boolean
    # check whether the number of nodes explored exceeds resource limit
    
    time_limit(start_time, limit)
    # returns boolean
    # checks weather time elapsed exceeds resource limit
    
    Functions changed:
    main()
    # added debug cases
    # added board_matrix, stores board as matrix rather than dictionary
    # print suggested path from path_finder()
    
    heuristic(coords,goal)
    # implemented new heuristic:
    # heuristic = ceiling of half of total distance of pieces to goal + sum of distances each pair of pieces need to move to get to favorable jumping position + number of pieces
    # pieces are paired because that is the most efficient way for them to leave the board (jumping over each other continuously)
    # distances pieces need to move is calculated from current location of pieces to a favorable jumping position
    # favorable jumping position is the hex where: for each piece, out of the two axes that are not goal axis we fix one and vary the other - the varied axes of our two pieces cross, and has the closest value to the goal value (since there will be two points where the varied axes cross)
    
    node_expander(goal)
    # added option for pieces to exit the board if their coordinates match the goal value on the goal axis
    # generalized board radius constraint (the one that keeps pieces jumping off the board)
    # added condition to prevent pieces from moving to a location where a piece exists in the path_finder projection
    # when goal state is found, it is now also added to explored_states. this is critical for returning the path
    
    path_finder(goal)
    # added two types of resource limits (node limit and time limit)
    # added path_found handling (called "Goal found" in the function)
    # added return values (path, or None if no path found)
    
    Functions removed:

--- source/Algorithm.py
+++ source/Algorithm.py
@@ -2,6 +2,9 @@
 import queue
 import math
 import operator
+import time
+import itertools
+
 
 import Formatting       # user defined class in same directory
 
@@ -15,15 +18,21 @@ explored_states = {}                        # key: coordinate tuple
 fringe_nodes = queue.PriorityQueue()        # nodes that are adjacent to explored nodes, ordered by f(n)
 min_f = [None, None]                        # node with minimum f(n). min_f[0] holds f(n) value, min_f[1] holds state
 
+matrix_size = board_radius * 2 + 1
+board_matrix = [[None for x in range(matrix_size)] for y in range(matrix_size)]
+
 
 def main():
-    global board, explored_states, fringe_nodes
+    global board, board_matrix, explored_states, fringe_nodes
     # board key: coordinate tuple, data: piece color
-    board_input = {(-3, 0, 3):"r", (-3, 1, 2):"r", (-3, 2, 1):"r", (-3, 3, 0):"r"}
+    # board_input = {"(-3, 0, 3)":"r", "(-3, 1, 2)":"r", "(-3, 2, 1)":"r", "(-3, 3, 0)":"r"}
     # board_input = {(-3, 0, 3): "r", (-3, 1, 2): "r", (-3, 2, 1): "r", (-3, 3, 0): "r", (-2, -1, 3): "block", (-2, 0, 2): "block", (-2, 1, 1): "block"}
     # board_input = {(-3, 0, 3):"r"}
-    # board_input = {(-3, 0, 3): "r", (-3, 1, 2): "r", (-2, -1, 3): "block"}
+    board_input = {"(-3, 0, 3)": "r", "(-3, 1, 2)": "r", "(-2, -1, 3)": "block"}
     board.update(board_input)
+
+    # converting board to matrix allows for more efficient code (can access by axes)
+    board_matrix = dict_to_matrix(board)
 
     global q, r, s
     # h = heuristic(((0,0,0),(-1,0,3)), (q,-3))
@@ -31,10 +40,11 @@ def main():
 
     goal = (q,3)
 
-    fringe_nodes.put((24, 0, ((-3, 0, 3), (-3, 1, 2), (-3, 2, 1), (-3, 3, 0)), None))
-    # fringe_nodes.put((24, 0, ((-3, 0, 3), (-3, 1, 2)), None))
+    # fringe_nodes.put((24, 0, ((-3, 0, 3), (-3, 1, 2), (-3, 2, 1), (-3, 3, 0)), None))
+    fringe_nodes.put((0, 0, ((-3, 0, 3), (-3, 1, 2)), None))
     # fringe_nodes.put((3, 0, ((-3, 0, 3),), None))
-    path_finder(goal)
+    path = path_finder(goal)
+    print("# Path: ", path)
     # node_expander(goal)
 
     # while not fringe_nodes.empty():
@@ -47,19 +57,74 @@ def main():
 #   coordinate tuple includes 3 axes i.e. q,r,s where s is derived from -q-r
 # goal is single tuple consisting of 2 elements: axis (i.e. q,r,s) and value (i.e. -3,3)
 def heuristic(coords, goal):
+    global board_matrix, q, r, s
+    num_pieces = len(coords)
+    axes = [q, r, s]
     total_distance = 0      # sum of shortest distances between pieces and their goal
     goal_axis = goal[0]     # axis perpendicular to the side our pieces have to move to
     goal_value = goal[1]    # either -n or n, where n is the radius of the hexagonal board
-                            #   distinguishes between the two sides perpendicular to the goal_axis
+    #   distinguishes between the two sides perpendicular to the goal_axis
 
     for coord in coords:
         piece_distance = abs(coord[goal_axis] - goal_value)   # position of piece of goal_axis
         total_distance += piece_distance
 
-    # modifier on heuristic value "h(n)"
-    h = math.ceil(total_distance * 1/2) # set to 1/2 to account for pieces moving 2 squares by jumping
-                                        # int and +1 acts as ceiling function - since you cannot jump off the board
+    if len(coords) > 1:
+        # keep track of steps between pieces
+        piece_separation = [[None for i in range(num_pieces)] for j in range(num_pieces)]
+        # since we want to move toward one axis (increase or decrease axis value), other two axes must be inversely affected
+        axes.remove(goal_axis)
+        # p1 and p2 are pieces
+        for p1 in range(len(coords)):
+            for p2 in range(p1 + 1, len(coords)):
+                y_dist = abs(coords[p1][axes[0]] - coords[p2][axes[0]])
+                z_dist = abs(coords[p1][axes[1]] - coords[p2][axes[1]])
+                # -1 because one piece reaches cross, the other piece sits behind it (from the goal)
+                piece_separation[p1][p2] = y_dist + z_dist - 1
 
+        print(piece_separation)
+        # combinations = [[[[piece_separation[p1][p2] + piece_separation[p3][p4]
+        #                    for p4 in range(len(piece_separation))if piece_separation[p1][p2] is not None
+        #                    and piece_separation[p3][p4] is not None
+        #                    and p4 not in [p3, p2, p1]]
+        #                   for p3 in range(len(piece_separation)) if p3 not in [p2, p1]]
+        #                  for p2 in range(len(piece_separation))if p2 not in [p1]]
+        #                 for p1 in range(len(piece_separation))]
+
+        combinations = [[piece_separation[p1][p2]
+                         for p2 in range(len(piece_separation)) if piece_separation[p1][p2] is not None and p2 != p1]
+                        for p1 in range(len(piece_separation))]
+
+        flatten_combinations = combinations
+        for i in range(1):
+            flatten_combinations = list(itertools.chain.from_iterable(flatten_combinations))
+
+        minsum_separation = min(flatten_combinations)
+
+    # for i in range(len(piece_separation)):
+    #     for j in range(len(piece_separation[i])):
+    #         if i != j and piece_separation[i][j] is not None:
+    #             for k in range(len(piece_separation)):
+    #                 for l in range(len(piece_separation[k])):
+
+
+    # group every 2 pieces together and have them continually jump over each other to goal
+    # sum the pair separations of every combination of n pairs of pieces, where n is half of number of pieces (floored)
+    # returned as tuple
+    # print("GOOSE", piece_separation)
+    # combinations = tuple(itertools.combinations(piece_separation, math.floor(num_pieces/2)))
+    # for x in combinations:
+    #     print(x)
+    # sum_separation = map(sum, combinations)
+    # print(min(sum_separation), "DEBUG DUCK")
+
+    # # modifier on heuristic value "h(n)"
+    # h = total_distance * 1/2 # set to 1/2 to account for pieces moving 2 squares by jumping
+
+    # modifier on heuristic value "h(n)"
+        h = math.ceil(total_distance/2) + minsum_separation + num_pieces
+    else:
+        h = math.ceil(total_distance / 2) + num_pieces
     # h = total_distance  # debug
     print("h= ",h)
     return h
@@ -79,6 +144,9 @@ def node_expander(goal):
     g = node[1]                                                         # g = cost of reaching state
     state = node[2]                                                     # state = tuple of piece coordinates
     prev_state = node[3]
+
+    goal_axis = goal[0]     # axis perpendicular to the side our pieces have to move to
+    goal_value = goal[1]    # either -n or n, where n is the radius of the hexagonal board
 
     # stringification needed because strings are always identical if equivalent, tuples are not
     stringified_state = Formatting.tuple_to_string(state)
@@ -100,28 +168,40 @@ def node_expander(goal):
     for piece in state:
         possible_moves.clear()
 
+        # add option to leave board
+        if piece[goal_axis] == goal_value:
+            valid_moves.append((piece, None))
+            continue
+
         for move in moves:
             new_pos = tuple(map(operator.add, piece, move))  # new_pos is a place the piece can move to
             (q, r, s) = new_pos
-            if abs(q) <= 3 and abs(r) <= 3 and abs(s) <= 3:            # check that new_pos is in the hexagon
+            # check that new_pos is in the hexagon
+            if abs(q) <= board_radius and abs(r) <= board_radius and abs(s) <= board_radius:
                 possible_moves.append(new_pos)
 
         for move in possible_moves:         # recall that this is one piece's possible moves, not all moves for player
-            if move not in board:
+            if new_pos not in board and new_pos not in state:
                 valid_moves.append((piece, move))  # recall that this is list of player's all possible moves
 
     # explore successors of current node, iterate through valid moves
     for choice in valid_moves:
         # choice[0] is original position of piece, index is index of piece in node tuple
         index = state.index(choice[0])
-        # choice[1] is new position of the piece, next_node is the new state
-        next_state = state[:index] + (choice[1],) + state[index+1:]
+        # if piece not leaving board
+        if choice[1] is not None:
+            # choice[1] is new position of the piece, next_node is the new state
+            next_state = state[:index] + (choice[1],) + state[index+1:]
+        else:
+            next_state = state[:index] + state[index+1:]
 
         stringified_next = Formatting.tuple_to_string(next_state)
         if stringified_next not in explored_states:
             h = heuristic(coords=next_state, goal=goal)
 
             if h == 0:
+                explored_states[stringified_next] = Formatting.tuple_to_string(state)
+                print("DEBUG4", state)
                 # goal has been found
                 return next_state
 
@@ -144,29 +224,77 @@ def node_expander(goal):
 
 
 def path_finder(goal):
-    # resource limit (number of nodes explored)
-    limit = 10000
+    current = 0                         # keep track of node resource
+    start_time = time.time()            # keep track of time resource
 
-    while not fringe_nodes.empty() and limit > 0:
-        goal_found = node_expander(goal)
-        if goal_found is not False:
-            print("Goal found")
-            print(goal_found)
-            break
-        limit -= 1
-        print(next)
+    goal_found = False
+    # while time_limit(start_time, limit=2.5) is True:
+    while node_limit(current, limit=100) is True:
+        # run node_expander i number of times before checking time elapsed
+        i = 1000
+        while not fringe_nodes.empty() and i > 0:
+            goal_found = node_expander(goal)
+            if goal_found is not False:
+                print("# Goal found")
+                print("# ", goal_found)
+                node = Formatting.tuple_to_string(goal_found)
+                print(explored_states)
 
-    # if goal not found, limit must have reached 0
-    if limit <= 0:
-        print("Resource limit")
+                path = []
+                while node != "root":
+                    path.append(node)
+                    node = explored_states[node]
+                path.reverse()
+                return path
+
+            i -= 1
+            print("# Next node")
+        current += 1
+
+    # if goal not found, must have reached resource limit
+    if goal_found is False:
+        print("# Resource limit")
 
         # node is same as implementation in node_expander
         node = fringe_nodes.get()
         f = node[0]
-        if min_f[0] <= f:
-            print(min_f[1])
+        if min_f[0] is not None and min_f[0] <= f:
+            print(min_f)
         else:
             print(node[2])
+
+    return None
+
+
+def dict_to_matrix(board):
+    size = board_radius * 2 + 1
+    # board_matrix = [size][size]
+    keys = board.keys()
+
+    for key in keys:
+        print(key, "DEBUG")
+        coord = Formatting.string_to_tuple(key)
+        q, r, s = coord
+
+        board_matrix[q][r] = board[key]
+
+    print(board_matrix)
+    return board_matrix
+
+
+def node_limit(current, limit):
+    if current <= limit:
+        return True
+    else:
+        return False
+
+
+def time_limit(start_time, limit):
+    if (time.time() - start_time) >= limit:
+        print("# TIME: ", time.time()-start_time)
+        return False
+    else:
+        return True
 
 
 if __name__ == "__main__":
