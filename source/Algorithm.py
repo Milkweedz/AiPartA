2f8e3b6c66fa9f19014e85ce4d2d0f4c2bb1d75f
Milkweedz <Milkweedz@hotmail.com>
2019-04-01 18:26:54 +1100

    Modified Algorithm.py
    Functions added:
    
    Functions changed:
    main()
    # added debug cases
    
    heuristic(coords, goal)
    # added combinations[] and sum_separations[], see comments for more details
    # store piece_distance (from goal) in array rather than just storing total distance
    # added piece_pairs[] that stores combinations of individual pieces
    # implemented code to generate all combinations of piece pairs in scenarios with range(1, 5) pieces
    # implemented code to determine sum_separations of each combination above
    # changed heuristic to be "h = math.ceil(total_distance/2) + minsum_separation + num_pieces" for even number of pieces
    # "h = math.ceil(total_distance/2) + minsum_separation + num_pieces + min(piece_distances)" for odd number of pieces
    
    node_expander(goal)
    # reworked code to put regular and jump moves into valid_moves[]
    
    path_finder(goal)
    # reworked code to always return a path
    # if path to goal is found, that path is returned
    # otherwise path to lowest f(n) node is returned
    # comment: given resource limit, path to goal may be found but not the lowest f(n)? No, lowest f(n) node is always explored
    
    Functions removed:
    
    # TODO: refactor code to have higher cohesion and better understandability

--- source/Algorithm.py
+++ source/Algorithm.py
@@ -25,10 +25,12 @@ board_matrix = [[None for x in range(matrix_size)] for y in range(matrix_size)]
 def main():
     global board, board_matrix, explored_states, fringe_nodes
     # board key: coordinate tuple, data: piece color
-    # board_input = {"(-3, 0, 3)":"r", "(-3, 1, 2)":"r", "(-3, 2, 1)":"r", "(-3, 3, 0)":"r"}
+    board_input = {"(-3, 0, 3)":"r", "(-3, 1, 2)":"r", "(-3, 2, 1)":"r", "(-3, 3, 0)":"r"}
+    # board_input = {"(0, 0, 0)": "r", "(0, 1, -1)": "r", "(0, 2, -2)": "r", "(0, -1, 1)": "r"}
     # board_input = {(-3, 0, 3): "r", (-3, 1, 2): "r", (-3, 2, 1): "r", (-3, 3, 0): "r", (-2, -1, 3): "block", (-2, 0, 2): "block", (-2, 1, 1): "block"}
-    # board_input = {(-3, 0, 3):"r"}
-    board_input = {"(-3, 0, 3)": "r", "(-3, 1, 2)": "r", "(-2, -1, 3)": "block"}
+    # board_input = {"(-3, 0, 3)":"r"}
+    # board_input = {"(-3, 0, 3)": "r", "(-3, 1, 2)": "r", "(-2, -1, 3)": "block"}
+    # board_input = {"(-3, 0, 3)": "r", "(-2, 0, 2)": "r", "(-2, -1, 3)": "block"}
     board.update(board_input)
 
     # converting board to matrix allows for more efficient code (can access by axes)
@@ -40,8 +42,10 @@ def main():
 
     goal = (q,3)
 
-    # fringe_nodes.put((24, 0, ((-3, 0, 3), (-3, 1, 2), (-3, 2, 1), (-3, 3, 0)), None))
-    fringe_nodes.put((0, 0, ((-3, 0, 3), (-3, 1, 2)), None))
+    fringe_nodes.put((0, 0, ((-3, 0, 3), (-3, 1, 2), (-3, 2, 1), (-3, 3, 0)), None))
+    # fringe_nodes.put((0, 0, ((0, 0, 0), (0, 1, -1), (0, 2, -2), (0, -1, 1)), None))
+    # fringe_nodes.put((0, 0, ((-3, 0, 3), (-3, 1, 2)), None))
+    # fringe_nodes.put((0, 0, ((-3, 0, 3), (-2, 0, 2)), None))
     # fringe_nodes.put((3, 0, ((-3, 0, 3),), None))
     path = path_finder(goal)
     print("# Path: ", path)
@@ -59,54 +63,90 @@ def main():
 def heuristic(coords, goal):
     global board_matrix, q, r, s
     num_pieces = len(coords)
+    # num_pairs = (num_pieces * (num_pieces-1))/2
+    combinations = []       # combinations of pairs of pieces
+    sum_separations = []    # sum of separations of pairs of pieces
     axes = [q, r, s]
-    total_distance = 0      # sum of shortest distances between pieces and their goal
+    piece_distances = []      # sum of shortest distances between pieces and their goal
     goal_axis = goal[0]     # axis perpendicular to the side our pieces have to move to
     goal_value = goal[1]    # either -n or n, where n is the radius of the hexagonal board
     #   distinguishes between the two sides perpendicular to the goal_axis
 
     for coord in coords:
         piece_distance = abs(coord[goal_axis] - goal_value)   # position of piece of goal_axis
-        total_distance += piece_distance
+        piece_distances.append(piece_distance)
 
-    if len(coords) > 1:
-        # keep track of steps between pieces
-        piece_separation = [[None for i in range(num_pieces)] for j in range(num_pieces)]
-        # since we want to move toward one axis (increase or decrease axis value), other two axes must be inversely affected
-        axes.remove(goal_axis)
-        # p1 and p2 are pieces
-        for p1 in range(len(coords)):
-            for p2 in range(p1 + 1, len(coords)):
-                y_dist = abs(coords[p1][axes[0]] - coords[p2][axes[0]])
-                z_dist = abs(coords[p1][axes[1]] - coords[p2][axes[1]])
-                # -1 because one piece reaches cross, the other piece sits behind it (from the goal)
-                piece_separation[p1][p2] = y_dist + z_dist - 1
+    total_distance = sum(piece_distances)
 
-        print(piece_separation)
-        # combinations = [[[[piece_separation[p1][p2] + piece_separation[p3][p4]
-        #                    for p4 in range(len(piece_separation))if piece_separation[p1][p2] is not None
-        #                    and piece_separation[p3][p4] is not None
-        #                    and p4 not in [p3, p2, p1]]
-        #                   for p3 in range(len(piece_separation)) if p3 not in [p2, p1]]
-        #                  for p2 in range(len(piece_separation))if p2 not in [p1]]
-        #                 for p1 in range(len(piece_separation))]
+    # keep track of steps between pieces
+    piece_separation = [[None for i in range(num_pieces)] for j in range(num_pieces)]
 
-        combinations = [[piece_separation[p1][p2]
-                         for p2 in range(len(piece_separation)) if piece_separation[p1][p2] is not None and p2 != p1]
-                        for p1 in range(len(piece_separation))]
+    piece_pairs = []
 
-        flatten_combinations = combinations
-        for i in range(1):
-            flatten_combinations = list(itertools.chain.from_iterable(flatten_combinations))
+    # since we want to move toward one axis (increase or decrease axis value), other two axes must be inversely affected
+    axes.remove(goal_axis)
+    # p1 and p2 are pieces
+    for p1 in range(len(coords)):
+        for p2 in range(p1+1, len(coords)):
+            piece_pairs.append([p1, p2])
 
-        minsum_separation = min(flatten_combinations)
+            y_dist = abs(coords[p1][axes[0]] - coords[p2][axes[0]])
+            z_dist = abs(coords[p1][axes[1]] - coords[p2][axes[1]])
+            # -1 because one piece reaches cross, the other piece sits behind it (from the goal)
+            piece_separation[p1][p2] = y_dist + z_dist - 1
+
+    # brittle, only applies to < 4 pieces
+    for i in range(len(piece_pairs)):
+        for j in range(i, len(piece_pairs)):
+            pieces = [piece_pairs[i][0], piece_pairs[i][1], piece_pairs[j][0], piece_pairs[j][1]]
+            # no duplicates
+            if len(pieces) == len(set(pieces)):
+                combinations.append([piece_pairs[i], piece_pairs[j]])
+
+    if len(combinations) == 0:
+        for i in range(len(piece_pairs)):
+            combinations.append([piece_pairs[i]])
+
+    # combinations = list(itertools.combinations(piece_pairs, int(num_pieces/2)))
+
+    for i in range(len(combinations)):
+        sum_separation = 0
+        for j in range(len(combinations[i])):
+            p1 = combinations[i][j][0]
+            p2 = combinations[i][j][1]
+
+            separation = piece_separation[p1][p2]
+            sum_separation += separation
+        sum_separations.append(sum_separation)
+
+    # combinations = [[[[piece_separation[p1][p2] + piece_separation[p3][p4]
+    #                    for p4 in range(len(piece_separation))if piece_separation[p1][p2] is not None
+    #                    and piece_separation[p3][p4] is not None
+    #                    and p4 not in [p3, p2, p1]]
+    #                   for p3 in range(len(piece_separation)) if p3 not in [p2, p1]]
+    #                  for p2 in range(len(piece_separation))if p2 not in [p1]]
+    #                 for p1 in range(len(piece_separation))]
+
+    # combinations = [[piece_separation[p1][p2]
+    #                  for p2 in range(len(piece_separation)) if piece_separation[p1][p2] is not None and p2 != p1]
+    #                 for p1 in range(len(piece_separation))]
+
+    # flatten_combinations = combinations
+    # for i in range(1):
+    #     flatten_combinations = list(itertools.chain.from_iterable(flatten_combinations))
+    #
+    # minsum_separation = min(flatten_combinations)
+
+    if len(sum_separations) == 0:
+        minsum_separation = 0
+    else:
+        minsum_separation = min(sum_separations)
 
     # for i in range(len(piece_separation)):
     #     for j in range(len(piece_separation[i])):
     #         if i != j and piece_separation[i][j] is not None:
     #             for k in range(len(piece_separation)):
     #                 for l in range(len(piece_separation[k])):
-
 
     # group every 2 pieces together and have them continually jump over each other to goal
     # sum the pair separations of every combination of n pairs of pieces, where n is half of number of pieces (floored)
@@ -122,11 +162,13 @@ def heuristic(coords, goal):
     # h = total_distance * 1/2 # set to 1/2 to account for pieces moving 2 squares by jumping
 
     # modifier on heuristic value "h(n)"
+    if num_pieces%2 == 0:
         h = math.ceil(total_distance/2) + minsum_separation + num_pieces
     else:
-        h = math.ceil(total_distance / 2) + num_pieces
+        h = math.ceil(total_distance/2) + minsum_separation + num_pieces + min(piece_distances)
+    # h = math.ceil(total_distance/2) + num_pieces
     # h = total_distance  # debug
-    print("h= ",h)
+    print("h= ", h)
     return h
 
 
@@ -152,7 +194,8 @@ def node_expander(goal):
     stringified_state = Formatting.tuple_to_string(state)
 
     # if already explored, discard node
-    if stringified_state in explored_states: return False
+    if stringified_state in explored_states:
+        return False
 
     # add state to explored states
     if prev_state is None:
@@ -161,7 +204,7 @@ def node_expander(goal):
         stringified_prev = Formatting.tuple_to_string(prev_state)
         explored_states[stringified_state] = stringified_prev
 
-    moves = [(1,-1,0),(1,0,-1),(0,1,-1),(-1,1,0),(-1,0,1),(0,-1,1)]     # unit vectors of a piece's possible moves
+    unit_moves = [(1,-1,0),(1,0,-1),(0,1,-1),(-1,1,0),(-1,0,1),(0,-1,1)]     # unit vectors of a piece's possible moves
     possible_moves = []                                                 # list of a piece's possible moves
     valid_moves = []                        # list of all possible moves for this turn, minus ones that are not legal
 
@@ -173,16 +216,25 @@ def node_expander(goal):
             valid_moves.append((piece, None))
             continue
 
-        for move in moves:
-            new_pos = tuple(map(operator.add, piece, move))  # new_pos is a place the piece can move to
+        for unit_move in unit_moves:
+            new_pos = tuple(map(operator.add, piece, unit_move))  # new_pos is a place the piece can move to
             (q, r, s) = new_pos
-            # check that new_pos is in the hexagon
-            if abs(q) <= board_radius and abs(r) <= board_radius and abs(s) <= board_radius:
-                possible_moves.append(new_pos)
+            stringified_new_pos = Formatting.tuple_to_string(new_pos)
 
-        for move in possible_moves:         # recall that this is one piece's possible moves, not all moves for player
-            if new_pos not in board and new_pos not in state:
-                valid_moves.append((piece, move))  # recall that this is list of player's all possible moves
+            # check if any piece on there (since beginning of the turn or in the simulation)
+            if stringified_new_pos in board or new_pos in state:
+                new_pos = tuple(map(operator.add, new_pos, unit_move))  # move again = jump over piece
+                (q, r, s) = new_pos
+                stringified_new_pos = Formatting.tuple_to_string(new_pos)
+
+            if stringified_new_pos in board or new_pos in state:
+                continue
+
+            # skip move if it puts piece out of board
+            if max(abs(q), abs(r), abs(s)) > board_radius:
+                continue
+
+            valid_moves.append((piece, new_pos))
 
     # explore successors of current node, iterate through valid moves
     for choice in valid_moves:
@@ -201,14 +253,13 @@ def node_expander(goal):
 
             if h == 0:
                 explored_states[stringified_next] = Formatting.tuple_to_string(state)
-                print("DEBUG4", state)
                 # goal has been found
-                return next_state
+                return (g + 1, next_state)
 
             # f is estimated total path cost, h is predicted cost of remaining path, g is current path cost
             f = h + g
-            print(choice, f, g, next_state)
-            print(state)
+            print(choice, state)
+            print(f, g, next_state)
             fringe_nodes.put((f, g + 1, next_state, state))
 
             if min_f[0] is None or f <= min_f[0]:
@@ -216,8 +267,8 @@ def node_expander(goal):
                 min_f[1] = next_state
 
         else:
-            print("Already explored")
-            print(stringified_next)
+            print("# Already explored")
+            print("# ", stringified_next)
 
     # goal has not been found
     return False
@@ -227,43 +278,34 @@ def path_finder(goal):
     current = 0                         # keep track of node resource
     start_time = time.time()            # keep track of time resource
 
-    goal_found = False
-    # while time_limit(start_time, limit=2.5) is True:
-    while node_limit(current, limit=100) is True:
+    path_found = False
+    # while time_limit(start_time, limit=5) is True:
+    while node_limit(current, limit=10) is True:
         # run node_expander i number of times before checking time elapsed
         i = 1000
-        while not fringe_nodes.empty() and i > 0:
-            goal_found = node_expander(goal)
-            if goal_found is not False:
-                print("# Goal found")
-                print("# ", goal_found)
-                node = Formatting.tuple_to_string(goal_found)
-                print(explored_states)
+        while not fringe_nodes.empty() and i > 0 and (path_found is False or path_found[0] < min_f[0]):
+            print("# Next node")
+            path_found = node_expander(goal)
 
-                path = []
-                while node != "root":
-                    path.append(node)
-                    node = explored_states[node]
-                path.reverse()
-                return path
+            if path_found is not False:
+                print("# Path found")
+                node = Formatting.tuple_to_string(path_found[1])
 
             i -= 1
-            print("# Next node")
         current += 1
 
     # if goal not found, must have reached resource limit
-    if goal_found is False:
+    if path_found is False:
         print("# Resource limit")
+        node = Formatting.tuple_to_string(min_f[1])
 
-        # node is same as implementation in node_expander
-        node = fringe_nodes.get()
-        f = node[0]
-        if min_f[0] is not None and min_f[0] <= f:
-            print(min_f)
-        else:
-            print(node[2])
+    path = []
+    while node != "root":
+        path.append(node)
+        node = explored_states[node]
+    path.reverse()
 
-    return None
+    return path
 
 
 def dict_to_matrix(board):
